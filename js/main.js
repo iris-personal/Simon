62e51f6e0988315d2c6a25515411ecc5b5680d13
iris-personal <mary.iris.toland@gmail.com>
2022-07-01 18:12:19 -0500

    Added Audio Files

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ const COLOR_LOOKUP = {
     null: 'grey'
 }
 
+// TODO: find audio files & make sure work
+const audio1 = new Audio('https://freesound.org/s/3515/');
+const audio2 = new Audio('https://freesound.org/s/3516/');
+const audio3 = new Audio('https://freesound.org/s/3517/');
+const audio4 = new Audio('https://freesound.org/s/3518/');
+
 const AUDIO_LOOKUP = {
     1: `${audio1}`,
     2: `${audio2}`,
@@ -15,26 +21,20 @@ const AUDIO_LOOKUP = {
     4: `${audio4}`,
 } 
 
-// TODO: find audio files
-const audio1 = new Audio('');
-const audio2 = new Audio('');
-const audio3 = new Audio('');
-const audio4 = new Audio('');
-
 /*----- app's state (variables) -----*/
-let board; // array of 4 elements 
-let loser; // boolean
+let board; // array of 4 elements
+let loser; 
 let gameStatus; // null -> game in progress
 let ignoreClick; // ignore clicks while progression plays
 let timing; //
 
 /*----- cached element references -----*/
 const blockEls = [...document.querySelectorAll('#board > div')];
-const msgEl = document.querySelector('h2');
+const msgEl = document.getElementById('msg');
 const replayBtn = document.querySelector('button');
 
 /*----- event listeners -----*/
-
+document.querySelector('main').addEventListener('click', handleChoice);
 
 /*----- functions -----*/
 init();
@@ -43,7 +43,6 @@ function init() {
     board = [
         null, null, null, null
     ];
-    loser = false;
     gameStatus = null;
     ignoreClick = false;
     render();
@@ -59,8 +58,25 @@ function render() {
 
 function renderMessage() {
     if (gameStatus === null) {
-        msgEl.innerHTML = 'Simon says follow my lead';
+        msgEl.innerHTML = 'Simon says, "follow my lead"!';
       } else  {
         msgEl.textContent = 'Too bad... you could NOT keep up!';
       }
+}
+
+function handleChoice(evt) {
+    //guards
+    if (evt.target.tagName !== 'BUTTON' || ignoreClick) return;
+    ignoreClick = true;
+    loser = '';
+    render();
+    loser = getLoser();
+    ignoreClick = false;
+    render();
+}
+
+function getRandomColor(cb) {
+    const color = Object.keys(COLOR_LOOKUP);
+    const rndIdx = Math.floor(Math.random() * color.length);
+    return color[rndIdx];
 }
